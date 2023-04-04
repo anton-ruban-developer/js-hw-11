797ba671865638a33c803babc8c58172445395f7
Anton <124812499+anton-ruban-developer@users.noreply.github.com>
2023-04-04 23:56:11 +0300

    Added new files
    start

---
+++ js/tasks.js
@@ -0,0 +1,106 @@
+const random = (min, max) => {
+    const rand = min + Math.random() * (max - min + 1);
+    return Math.floor(rand);
+  };
+  
+  const btn = document.querySelector("#btn");
+  btn.addEventListener("mouseenter", () => {
+    btn.style.left = `${random(0, 90)}%`;
+    btn.style.top = `${random(0, 90)}%`;
+  });
+  
+  btn.addEventListener("click", () => {
+    alert("Congrats! You did it!!!!");
+  });
+  
+  const btn1 = document.querySelector("#btn1");
+  btn1.addEventListener("mouseenter", () => {
+    btn1.style.left = `${random(0, 90)}%`;
+    btn1.style.top = `${random(0, 90)}%`;
+  });
+  
+  btn1.addEventListener("click", () => {
+    alert("Congrats! You did it!!!!");
+  });
+  
+  const btn2 = document.querySelector("#btn2");
+  btn2.addEventListener("mouseenter", () => {
+    btn2.style.left = `${random(0, 90)}%`;
+    btn2.style.top = `${random(0, 90)}%`;
+  });
+  
+  btn2.addEventListener("click", () => {
+    alert("Congrats! You did it!!!!");
+  });
+  
+  const btn3 = document.querySelector("#btn3");
+  btn3.addEventListener("mouseenter", () => {
+    btn3.style.left = `${random(0, 90)}%`;
+    btn3.style.top = `${random(0, 90)}%`;
+  });
+  
+  btn3.addEventListener("click", () => {
+    alert("Congrats! You did it!!!!");
+  });
+  
+  const btn4 = document.querySelector("#btn4");
+  btn4.addEventListener("mouseenter", () => {
+    btn4.style.left = `${random(0, 90)}%`;
+    btn4.style.top = `${random(0, 90)}%`;
+  });
+  
+  btn4.addEventListener("click", () => {
+    alert("Congrats! You did it!!!!");
+  });
+
+
+  const btn5 = document.querySelector("#btn5");
+  btn5.addEventListener("mouseenter", () => {
+    btn5.style.left = `${random(0, 90)}%`;
+    btn5.style.top = `${random(0, 90)}%`;
+  });
+  
+  btn5.addEventListener("click", () => {
+    alert("Congrats! You did it!!!!");
+  });
+  
+
+  const btn6 = document.querySelector("#btn6");
+  btn6.addEventListener("mouseenter", () => {
+    btn6.style.left = `${random(0, 90)}%`;
+    btn6.style.top = `${random(0, 90)}%`;
+  });
+  
+  btn6.addEventListener("click", () => {
+    alert("Congrats! You did it!!!!");
+  });
+
+  const btn7 = document.querySelector("#btn7");
+  btn7.addEventListener("mouseenter", () => {
+    btn7.style.left = `${random(0, 90)}%`;
+    btn7.style.top = `${random(0, 90)}%`;
+  });
+  
+  btn7.addEventListener("click", () => {
+    alert("Congrats! You did it!!!!");
+  });
+
+  const btn8 = document.querySelector("#btn8");
+  btn8.addEventListener("mouseenter", () => {
+    btn8.style.left = `${random(0, 90)}%`;
+    btn8.style.top = `${random(0, 90)}%`;
+  });
+  
+  btn8.addEventListener("click", () => {
+    alert("Congrats! You did it!!!!");
+  });
+
+  const btn9 = document.querySelector("#btn9");
+  btn9.addEventListener("mouseenter", () => {
+    btn9.style.left = `${random(0, 90)}%`;
+    btn9.style.top = `${random(0, 90)}%`;
+  });
+  
+  btn9.addEventListener("click", () => {
+    alert("Congrats! You did it!!!!");
+  });
